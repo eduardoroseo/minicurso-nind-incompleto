c0e0cee8c2f9654a3a10e40c318ac56bc7e95f6e
Eduardo Roseo <eduardoroseo@gmail.com>
2018-07-21 14:20:11 -0300

    Atualizando projeto

--- index/bootstrap.php
+++ index/bootstrap.php
@@ -1,13 +1,30 @@
 <?php
-require "../app/model/CervejaModel.php";
-require "../app/controller/CervejaController.php";
+
+importarClasses("../app/model");
+importarClasses("../app/controller");
 
 $urlCompleta = $_GET['url'];
 
 $url = explode('/', $urlCompleta);
 
 $classe = ucfirst($url[0])."Controller";
+
+if(!class_exists($classe)){
+    echo "<h1> Página não encontrada </h1>";
+    return;
+}
+
 $action = "action".ucfirst($url[1]);
 
 $obj = new $classe();
 $obj->$action();
+
+function importarClasses($caminho)
+{
+    foreach (scandir($caminho) as $nomeDoArquivo){
+        $path = $caminho . '/' . $nomeDoArquivo;
+        if (is_file($path)){
+            require $path;
+        }
+    }
+}
